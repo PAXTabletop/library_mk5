d6fc43e15139f1487d81fb5ddab3976a306ecbb1
Mebibyte <duelgamer@gmail.com>
2018-03-20 23:52:49 -0700

    - Improve Search

--- app/assets/javascripts/games.js
+++ app/assets/javascripts/games.js
@@ -2,7 +2,13 @@
 // All this logic will automatically be available in application.js.
 $(document).ready(function(){
 
-    $('#g-search').on('change', function(){
+    $('#g-search input').keydown(function(e) {
+        if (e.keyCode == 13) {
+            $('#g-search').submit();
+        }
+    });
+
+    $('#g-search').on('submit', function(){
         $.get('/games', $(this).serialize(), null, 'script');
     });
 
